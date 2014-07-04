08fafd4ce4b51c8c9ffb3010ca33725bac8dd6bd
Matt Rzepka <matthewrzepka@gmail.com>
2014-07-04 13:07:39 -0400

    now gets more data

--- get_data.js
+++ get_data.js
@@ -1,43 +1,102 @@
-$(document).ready(function() {
-	
+var btn = document.getElementById('getRequestButton');
+btn.addEventListener('click', main, false);
+
+
+function main () {
 	//key is the dev id key, riot api and wmata api are the api's I'm trying to access.
-	var key = 'c841951f-9130-4a73-8b15-86fac8e5fb49';
-	var riot_api = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/ObeseNightmare?api_key=" + key;
-	var wmata_api = "http://api.wmata.com/Rail.svc/json/jLines?api_key=kfgpmgvfgacx98de9q3xazww";
-	
-	
-	alert( "ready" );
-	
-	//button onclick
-	$( "#getInfo" ).submit(function(form){
-	
-	var summoner = form.summonerName.value;
-	var region = form.region.value;
-	
-	alert( summoner +" , "+ region );
-	
-	//Get request via ajax
-	var request = $.ajax({
-		type: "GET",
-		url: riot_api
-	});
-		
-	//Once get request is finished, put all the objects and id's into the paragraph
-	request.done(function(msg) {
-			$.each(msg, function(idx, obj){
-				console.log( obj );
-				$.each(obj, function(i, o){
-					
-					//log things just because
-					console.log(i + " : " + o);
-					
-					//actual append call
-					$( "p" ).append( i+" : "+o+ '<br>' );
-				});
-			});
-		
-			//alert('done');
-	});
-	});
-	
-});
+	var key = '9910cbfe-cf9b-4c46-af93-8c75cb9b99a6';
+	var summToSearch = 'ObeseNightmare';
+	var region = 'na'
+	var baseApi = 'https://na.api.pvp.net'
+	var summApi = '/api/lol/' + region + '/v1.4/summoner/by-name/' + 
+				summToSearch + '?api_key=';
+
+	//Here are all the stats we'll need to calculate carriability
+	var deaths = [];
+	var assists = [];
+	var wards = [];
+	var wardsKilled = [];
+
+	var totDeaths = 0;
+	var totAssists = 0;
+	var totWards = 0;
+	var totWardsKilled = 0;
+
+	var i;
+
+
+
+
+	//Runs get request via JS, used to get the info needed
+	function getHttp( theUrl ) {
+		var xmlHttp = null;
+
+		xmlHttp = new XMLHttpRequest();
+		xmlHttp.open( "GET", theUrl, false);
+		xmlHttp.send( null );
+		return xmlHttp.responseText;
+	}
+
+	function getGameInfo( game ) {
+		var currWardsPlaced = typeof game.wardPlaced === 'undefined' ? 0 : game.wardPlaced;
+		var currVisionBought = typeof game.visionWardsBought === 'undefined' ? 0 : game.visionWardsBought;
+		var currWardsKilled = typeof game.wardKilled === 'undefined' ? 0 : game.wardKilled;
+		deaths.push(game.numDeaths);
+		assists.push(game.assists);
+		wards.push(currWardsPlaced + currVisionBought);
+		wardsKilled.push(currWardsKilled);
+		console.log('deaths: ' + game.numDeaths);
+		console.log('assists: ' + game.assists);
+		console.log('wards: ' + currWardsPlaced);
+		console.log('wards killed: ' + currWardsKilled);
+	}
+
+	//response is the object returned, parsing the string value returned from getHttp
+	var response = JSON.parse(getHttp( baseApi + summApi + key));
+	var summoner = response[summToSearch.toLowerCase()];
+	console.log(summoner['id']);
+
+	//We need the summoners id to get the recent games.
+	var gamesApi = '/api/lol/' + region + '/v1.3/game/by-summoner/' + 
+	summoner['id'] + '/recent?api_key=';
+
+	//Make call to get recent games
+	var response = JSON.parse(getHttp( baseApi + gamesApi + key));
+	var recentGames = response['games'];
+
+	console.log(recentGames);
+
+	//Get info from last 10 games
+	for (game in recentGames){
+		var curr = recentGames[game].stats;
+		getGameInfo( curr );
+	}
+
+	//Calculate totals of last 10 games
+	for (i = 0; i < 10; i += 1) {
+		totDeaths += deaths[i];
+		totAssists += assists[i];
+		totWards += wards[i];
+		totWardsKilled += wardsKilled[i];
+	}
+
+	var avgDeaths = totDeaths/10;
+	var avgAssists = totAssists/10;
+	var avgWards = totWards/10;
+	var avgWardsKilled = totWardsKilled/10;
+
+	console.log('avg deaths: ' + avgDeaths);
+	console.log('avg assists: ' + avgAssists);
+	console.log('avg wards: ' + avgWards);
+	console.log('avg wards killed: ' + avgWardsKilled);
+
+
+
+
+	console.log(response);
+
+	console.log(typeof response);
+
+};
+
+alert( "ready" );
